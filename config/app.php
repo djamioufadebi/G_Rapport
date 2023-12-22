d5ab1001866f5e2b2244146dcbba8eaaa60344f4
djamioufadebi <djamioufadebi1@gmail.com>
2023-12-22 12:07:53 +0100

    Nouvel enregistrement

--- config/app.php
+++ config/app.php
@@ -183,6 +183,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        //  'PDF' => Dompdf\Facades\Dompdf::class,
     ])->toArray(),
 
 ];
